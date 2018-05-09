76e0871ec5b7412b90e167f40ac5a4e165bd2c22
Gowe Wang <whygowe@gmail.com>
2018-05-09 15:28:16 +0800

    implement buffer for TxMongoPipeline

--- NEMUserCrawler/pipelines.py
+++ NEMUserCrawler/pipelines.py
@@ -9,9 +9,10 @@
 import logging
 import txmongo
 from pymongo.uri_parser import parse_uri
-from pymongo.errors import DuplicateKeyError
+from pymongo.errors import DuplicateKeyError, BulkWriteError
 from twisted.internet import defer
 from scrapy.exceptions import NotConfigured
+from pymongo import InsertOne, UpdateOne
 
 
 class TxMongoPipeline(object):
@@ -25,22 +26,23 @@ class TxMongoPipeline(object):
 
         self.buffer_size = buffer_size
         if buffer_size > 0:
-            raise NotImplementedError("Buffer is not implemented yet.")
-            # self.buffer = {}
-            # {'<collection_name>': [<item>, ...], ...}
+            self.buffer = {}
+            self.buffer_count = 0
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
             db_name=crawler.settings.get('MONGO_DB'),
-            buffer_size=crawler.settings.get('MONGO_BUFFER_SIZE', 0)
+            buffer_size=crawler.settings.getint('MONGO_BUFFER_SIZE', 0)
         )
 
     @defer.inlineCallbacks
     def open_spider(self, spider):
         try:
-            self.db_name = spider.database_name or self.db_name
+            # TODO: get db_name from `spider.name`
+            pass # TODO: temp hack for test
+            # self.db_name = spider.database_name or self.db_name
         except AttributeError:
             pass
         if self.db_name is None:
@@ -50,40 +52,72 @@ class TxMongoPipeline(object):
                     self.__class__.__name__)
             self.logger(e)
             raise NotConfigured(e)
+        self.logger.info("TxMongoPipeline activated, uri: {}, database: {}.".format(self.mongo_uri, self.db_name))
         self.connection = yield txmongo.connection.ConnectionPool(self.mongo_uri)
         self.db = self.connection[self.db_name]
 
+    @defer.inlineCallbacks
     def close_spider(self, spider):
-        return self.connection.disconnect()
+        yield self.flush_buffer()
+        yield self.connection.disconnect()
 
     @defer.inlineCallbacks
     def process_item(self, item, spider):
-        if self.buffer_size:
-            raise NotImplementedError("Buffer is not implemented yet.")
+        collection_name = item.collection_name if hasattr(item, "collection_name")\
+            else item.__class__.__name__
+        processed_item = dict(item)
+        try:  # to use the field name specified in `to__id` as `_id` in MongoDB
+            _id = processed_item.pop(item.to__id)
+        except KeyError:
+            # `to__id` not specified
+            _id = None
         else:
-            collection_name = item.collection_name if hasattr(item, "collection_name")\
-                else item.__class__.__name__
-            processed_item = dict(item)
-            try:  # to use the field name specified in `to__id` as `_id` in MongoDB
-                _id = processed_item.pop(item.to__id)
-            except KeyError:
-                _id = None
+            if _id is None:
+                # `to__id` specified, but the field specified by it not existing
+                self.logger.warn(
+                    "Item does not have the field to be used as `_id`: {}".format(item))
+            processed_item['_id'] = _id
+            # Now, the name of the field specified by `item.to__id` is replaced by the value of `item.to__id`.
+
+        # `upsert` here: denotes whether the insert operation is to use `insert_one` or `update` with `upsert=True`
+        # in the former case, DuplicateKeyError may be raised
+        upsert = _id is not None and hasattr(item, 'upsert') and item.upsert
+        # TODO: test error handling
+        if self.buffer_size:
+            # buffer enabled
+            if self.buffer_count >= self.buffer_size:
+                result = yield self.flush_buffer()
             else:
-                if _id is None:
-                    self.logger.warn(
-                        "Item does not have the field to be used as `_id`: {}".format(item))
-                processed_item['_id'] = _id
-            # `upsert` here: denotes whether the insert operation is to use `insert_one` or `update` with `upsert=True`
-            # in the former case, DuplicateKeyError may be raised
-            if _id is None or not (hasattr(item, 'upsert') and item.upsert):
+                operation = UpdateOne({'_id': _id}, {'$set': processed_item}, upsert=True) if upsert else InsertOne(processed_item)
+                self.buffer.setdefault(collection_name, []).append(operation)
+                self.buffer_count += 1
+        else:
+            # buffer disabled
+            if upsert:
                 try:
                     result = yield self.db[collection_name].insert_one(processed_item)
-                except DuplicateKeyError:
-                    self.logger.warn("DuplicateKeyError raised when handling {}: {}. "
-                                     "Consider using `to__id` with `upsert=True`".format(collection_name, processed_item))
-                else:
-                    result = None
+                except DuplicateKeyError as e:
+                    self.logger.warn("{!r} raised when handling {}: {}. "
+                                     "Consider using `to__id` with `upsert=True`".format(e, collection_name, processed_item))
+                    result = e
             else:
                 result = yield self.db[collection_name].update({'_id': _id}, processed_item, upsert=True)
-            spider.crawler.stats.inc_value('pipeline/txmongo/{}'.format(collection_name), spider=spider)
-            defer.returnValue(item)
+        spider.crawler.stats.inc_value(
+            'pipeline/txmongo/{}'.format(collection_name), spider=spider)
+        defer.returnValue(item)
+
+    @defer.inlineCallbacks
+    def flush_buffer(self):
+        results = []
+        buffer = self.buffer.copy() # execution flow switched to other coroutines when bulk write
+        self.buffer.clear()
+        self.buffer_count = 0
+        for collection_name, operations in buffer.items():
+            try:
+                result = yield self.db[collection_name].bulk_write(operations, ordered=False)
+                self.logger.debug("Buffer flushed, {} for collection {}: {}".format(len(operations), collection_name, result.bulk_api_result))
+            except BulkWriteError as e:
+                self.logger.error("{!r} when writing buffer: {}".format(e, e.details))
+                result = e.details
+                results.append(result)
+        defer.returnValue(results)
